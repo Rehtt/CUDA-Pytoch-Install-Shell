bec05644101155308b38ad37d83a701f7cd0b71b
Rehtt <dsreshiram@gmail.com>
2019-06-01 16:01:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! [ -r /root ];then
+    echo "需要root权限执行该脚本"
+    exit
+fi
+apt update
+apt upgrade -y
 #脚本绝对路径
 SHELL_FOLDER=$(dirname "$0")
 
@@ -10,101 +16,120 @@ OS_RELEASE=$DISTRIB_RELEASE                 #eg. 16.04
 OS_CODENAME=$DISTRIB_CODENAME               #eg. xenial
 OS_DESCRIPTION=$DISTRIB_DESCRIPTION         #eg. Ubuntu 16.04.6 LTS
 OS_ARCH=""
-if [ -n $(uname -a | grep x86_64) ];then
+if [ -n "$(uname -a | grep x86_64)" ] ;then
     OS_ARCH=x86_64
-elif [ -n $(uname -a | grep ppc64le) ];then
+elif [ -n "$(uname -a | grep ppc64le)" ] ;then
     OS_ARCH=ppc64le
 else
     echo "cuda仅支持x86_64和ppc64le"
 fi
 
-#判断是否有gcc
-if [ -s /usr/bin/gccc ] && [ -s /usr/bin/make ]
+#判断是否有gcc make
+if ! [ -s /usr/bin/gcc ] && ! [ -s /usr/bin/make ]
 then
-        echo "安装cuda..."
-        URL=""
-        KEY="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/7fa2af80.pub"
-        case "$OS" in
-            "Ubuntu" )
-                if [ "$OS_ARCH" == "ppc64le" ];then
-                    if [ "OS_RELEASE" == "18.04"];then
-                        URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/ppc64el/cuda-repo-ubuntu1804_10.0.130-1_ppc64el.deb"
-                    else
-                        echo "ppc64le只支持18.04"
-                        exit
-                    fi
+    apt install make gcc -y
+    if ! [ -s /usr/bin/gccc ] && ! [ -s /usr/bin/make ];then
+            echo "安装gcc make失败，检查网络重试"
+            exit
+    fi
+fi
+
+#cuda
+if ! [ -s /usr/local/cuda-10.0 ];then
+    echo "安装cuda..."
+    URL=""
+    KEY="/var/cuda-repo-<version>/7fa2af80.pub"
+    case "$OS" in
+        "Ubuntu" )
+            if [ "$OS_ARCH" == "ppc64le" ] ;then
+                if [ "OS_RELEASE" == "18.04"] ;then
+                    URL="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_ppc64el"
+                else
+                    echo "ppc64le只支持18.04"
+                    exit
                 fi
-                
-                case "$OS_RELEASE" in
-                    "18.04")
-                        URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
-                    ;;
-                    "16.04")
-                        URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb"
-                    ;;
-                    "14.04")
-                        URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_10.0.130-1_amd64.deb"
-                    ;;
-                    * )
-                        echo "ubuntu 支持18.04\16.04\14.04"
-                        exit
-                    esac
-            ;;
-            * )
-                echo "该脚本暂支持ubuntu"
-                exit
-            esac
-
-        wget $URL -O "/tmp/cuda.deb"
-        if [ -s "/tmp/cuda.deb" ];then
-            web_size=$(curl -L --head $URL | grep content-length | awk '{print $2}' | tr -cd "[0-9]")
-            file_size=$(ls -l /tmp/cuda.deb | awk '{print $5}')
-            if [ "0169$file_size" == "$web_size" ];then
-                dpkg -i /tmp/cuda.deb
-                apt-key adv --fetch-keys $KEY
-                apt update
-                apt install cuda
-            else
-                echo "下载失败，检查网络重试"
-                exit
-        else
-            echo "下载失败，检查网络重试"
-            exit
-        fi
-        if ! [ -e /usr/local/cuda-10.0 ];then
-            echo "cuda 安装失败"
-            exit
-        fi
-        echo "cuda安装完毕"
-else
-        apt install make gcc
-        if ! [ -s /usr/bin/gccc ] && ! [ -s /usr/bin/make ];then
-                echo "安装gcc make失败，检查网络重试"
-                exit
-        fi
-fi
-
-python3=/usr/bin/python3.
-#判断python版本是否是3.5版
-if [ -s $python3"6" ]
-then
-        echo "python3.6"
-elif [ -s $python3"5" ]
-then
-        if [ -s /usr/bin/pip3 ];then
-            pip3 install opencv-python
-            pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp35-cp35m-linux_x86_64.whl
-            pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp35-cp35m-linux_x86_64.whl
-            pip3 install cupy-cuda100
-            if [ -n "$(pip3 list|grep opencv)" ] && [ -n "$(pip3 list|grep torch)" ] && [ -n "$(pip3 list|grep torchvision)" ];then
-                echo "安装成功"
-            else
-                echo "安装失败"
             fi
-        else
-            apt install python3-pip -y
-        fi
-else
-    apt install python3 -y
+            
+            case "$OS_RELEASE" in
+                "18.04")
+                    URL="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
+                ;;
+                "16.04")
+                    URL="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
+                ;;
+                "14.04")
+                    URL="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
+                ;;
+                * )
+                    echo "ubuntu 支持18.04\16.04\14.04"
+                    exit
+                esac
+        ;;
+        * )
+            echo "该脚本暂支持ubuntu"
+            exit
+        esac
     
+    if ! [ -s /tmp/cuda.run ];then
+        wget $URL -O "/tmp/cuda.run"
+    fi
+    web_size=$(curl -L --head $URL | grep content-length | awk '{print $2}' | tr -cd "[0-9]")
+    file_size=$(ls -l /tmp/cuda.run | awk '{print $5}')
+    if [ "0169$file_size" == "$web_size" ];then
+        chmod +x /tmp/cuda.run
+        bash /tmp/cuda.run
+    else
+        rm /tmp/cuda.run
+        echo "下载失败，检查网络重试"
+    fi
+
+    if ! [ -s /usr/local/cuda-10.0 ];then
+        echo "cuda 安装失败"
+        exit
+    fi
+    echo "cuda安装完毕"
 fi
+
+#检查python3是否安装
+if ! [ -s /usr/bin/python3 ] || ! [ -s /usr/bin/pip3 ];then
+    apt install python3 -y
+    apt install python3-pip -y
+fi
+#python3.x版本
+v=0
+if [ -n "$(python3 -V|grep 3.5)" ];then
+    v=35
+elif [ -n "$(python3 -V|grep 3.6)" ];then
+    v=36
+elif [ -n "$(python3 -V|grep 3.7)" ];then
+    v=37
+fi
+#安装必要的文件
+apt install libsm-dev -y
+apt install libxrender-dev -y
+# apt install libcublas9.1 -y
+pip_install(){
+    pip_yuan="https://pypi.tuna.tsinghua.edu.cn/simple"
+    pip3 install -i $pip_yuan $1
+    if [ -z "$(pip3 list|grep $name)" ]
+    then
+        echo "${name}安装失败"
+        echo "是否重试？ [Y/n]"
+        read r
+        if [ "$r" == "Y" ] || [ "$r" == "y" ] || [ -z "$r" ]
+        then
+            pip_install $name
+        else
+            exit
+        fi
+    fi
+}
+pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp${v}-cp${v}m-linux_x86_64.whl
+pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp${v}-cp${v}m-linux_x86_64.whl
+pip_name=(opencv-python cupy-cuda100 scikit-image torchnet)
+for name in ${pip_name[*]}
+do
+    pip_install $name
+done
+
+ldconfig /usr/local/cuda-10.0/lib64/
